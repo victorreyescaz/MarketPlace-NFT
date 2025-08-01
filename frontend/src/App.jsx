0a12aba9b7dc991921dced8dd33d78bb907f63b3
Víctor Reyes <victorreyes.caz@gmail.com>
2025-08-01 11:20:15 +0200

    Para subirlo a Github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <header className="p-4 bg-gray-100 flex justify-between">
-      <h1 className="text-xl font-semibold text-blue-600">MarketPlace NFT</h1>
+      <h1 className="text-xl font-bold text-blue-600">MarketPlace NFT</h1>
       {/* Botón de conexión */}
       <appkit-button />
     </header>
