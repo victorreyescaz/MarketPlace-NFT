7ed28bee3caf86aec6d5b7fbaac90ef52f2abf2a
Víctor Reyes <victorreyes.caz@gmail.com>
2025-08-27 11:11:14 +0200

    Comentando scripts

--- Marketplace/contracts/NFT.sol
+++ Marketplace/contracts/NFT.sol
@@ -2,17 +2,18 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // Guarda URIs por token en storage
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; // Indexa y permite enumerar todos los tokens y los de cada dueño
+import "@openzeppelin/contracts/access/Ownable.sol"; // onlyOwner
 
 contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
-    uint256 public tokenCount;
+    uint256 public tokenCount; // Contador de tokens
 
     constructor(
         address initialOwner
     ) ERC721("NFT Marketplace", "NFTM") Ownable(initialOwner) {}
 
+    // Funcion para mintear NFTs
     function mint(string memory _tokenURI) external returns (uint256) {
         tokenCount++;
         _safeMint(msg.sender, tokenCount);
@@ -20,9 +21,9 @@ contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
         return tokenCount;
     }
 
-    // ----------------- Overrides OZ v5 -----------------
+    // ----------------- Overrides OZ v5 -------------- --- Hay que implementar estos overrides ya que de los contratos que heredamos tenemos herencia múltiple ---
 
-    // Enumerable integra su contabilidad a través de estos hooks nuevos
+    // Hook de ERC721Enumerable,
     function _update(
         address to,
         uint256 tokenId,
@@ -31,6 +32,7 @@ contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
         return super._update(to, tokenId, auth);
     }
 
+    // Hook para separar el incremento de balances de flujo de _update
     function _increaseBalance(
         address account,
         uint128 value
@@ -38,7 +40,7 @@ contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
         super._increaseBalance(account, value);
     }
 
-    // tokenURI viene de URIStorage; combina con ERC721
+    // tokenURI viene de URIStorage, combina con ERC721
     function tokenURI(
         uint256 tokenId
     ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
@@ -56,4 +58,11 @@ contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
+
+    //     function _burn(uint256 tokenId)
+    //     internal
+    //     override(ERC721, ERC721URIStorage)
+    //     {
+    //     super._burn(tokenId); // limpia también el tokenURI en storage
+    //     }
 }
